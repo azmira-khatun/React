609c8f4fbe61e4e50e75f4d5945058a2dbe50cd6
azmira-khatun <azmiragrn@gmail.com>
2025-09-18 13:01:08 +0600

    table

---
+++ my-first-react-project/ThirdPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ThirdPage = () => {
+    const a="Miras Project";
+    const b="Thanks";
+  return (
+    <div>
+<h1 style={{backgroundColor:'yellow'}}>Hi Its {a}</h1>
+<h2>{b}👏👏</h2>
+    </div>
+  )
+}
+
+export default ThirdPage
---
+++ my-first-react-project/src/Table.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+       <div>
+        <table border={1}>
+            <thead>
+                <th>Name</th>
+                <th>Contact</th>
+                <th>Address</th>
+
+            </thead>
+            <tbody>
+                <td>Azmira</td>
+                <td>01908976</td>
+                <td>Dhaka</td>
+             </tbody>
+             <tbody>
+                <td>Sharmin</td>
+                <td>01900006</td>
+                <td>Dhanmondi</td>
+             </tbody>
+
+
+
+        </table>
+        </div> 
+    </div>
+  )
+}
+
+export default Form
