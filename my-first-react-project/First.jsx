aec464b3d192fa798955c615fb3673612333dff3
azmira-khatun <azmiragrn@gmail.com>
2025-09-18 12:04:27 +0600

    mm

---
+++ my-first-react-project/First.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const First = () => {
+
+  return (
+    <div>
+        <div>
+            <h1>This is Header side</h1>
+        </div>
+    </div>
+  )
+}
+
+export default First
--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import First from '../First'
+import SecondPage from './SecondPage'
 
 const App = () => {
 
 
 
 
-  
+
   return (
     <div>
       <h1>Hello</h1>
+      <First/>
+     
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique soluta voluptatum voluptas harum enim, corporis dignissimos ducimus necessitatibus, cupiditate sed cumque. Quisquam perspiciatis facilis blanditiis reprehenderit nemo laboriosam odio sequi unde quam magnam. Quisquam cupiditate, earum neque et veritatis sequi corrupti ea quia quae totam consequuntur explicabo, porro nesciunt modi illum quod ipsam praesentium reiciendis debitis! Natus inventore fugiat quo quasi, blanditiis a, culpa amet officiis incidunt ut distinctio repudiandae quas. Deleniti repudiandae repellendus, incidunt ipsam ut perspiciatis temporibus quia iure eaque facere error deserunt dolorum cum veniam delectus culpa numquam reprehenderit doloribus et! Aspernatur id eos architecto ex quisquam.</p>
+    <SecondPage/>
+    
     </div>
   )
 }
