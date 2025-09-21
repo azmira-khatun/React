960bcae987009f612d5491e7426aaa6b5d51a8a7
azmira-khatun <azmiragrn@gmail,com>
2025-09-21 07:13:22 +0600

    mm

--- add-link/src/App.jsx
+++ add-link/src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+      <Nav/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/contact-us" element={<ContactPage />} />
